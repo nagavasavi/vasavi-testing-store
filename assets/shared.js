9080fdd63b0c2ffa921ae6deb1c9c61d8a706904
nagavasavi <90404709+nagavasavi@users.noreply.github.com>
2024-07-05 15:40:34 +0530

    cart drawer implementation (#1)
    * cart drawer implementation
    
    * Initial buid setup

---
+++ assets/shared.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkshoptrade_Shopify_Development"] = self["webpackChunkshoptrade_Shopify_Development"] || []).push([["shared"],{
+
+/***/ "./js/components/react-wrapper.js":
+/*!****************************************!*\
+  !*** ./js/components/react-wrapper.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ "./node_modules/react-dom/client.js");
+/* harmony import */ var json_6__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! json-6 */ "./node_modules/json-6/dist/index.js");
+/* harmony import */ var json_6__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(json_6__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((Component, container, propsEl) => {
+  console.log("pppppp");
+  //component is a pre-compiled class
+  //container is where you want to inject component
+  //propEl pass prop when injecting
+  var appContainer = document.querySelectorAll(container);
+  var singlePropsEl = document.querySelector(propsEl);
+  var props = {};
+  if (singlePropsEl) {
+    props = json_6__WEBPACK_IMPORTED_MODULE_2___default().parse(singlePropsEl === null || singlePropsEl === void 0 ? void 0 : singlePropsEl.innerHTML) || {};
+  }
+  return [...appContainer].map(appTarget => {
+    var initialized = appTarget.dataset.initialized || false;
+    if (appTarget && !initialized) {
+      if (!singlePropsEl) {
+        if (propsEl) {
+          var _appTarget$querySelec;
+          props = {};
+          props = json_6__WEBPACK_IMPORTED_MODULE_2___default().parse((_appTarget$querySelec = appTarget.querySelector(propsEl)) === null || _appTarget$querySelec === void 0 ? void 0 : _appTarget$querySelec.innerHTML) || {}; //get json from the script id 
+        }
+      }
+      appTarget.dataset.initialized = true;
+      var root = (0,react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot)(appTarget);
+      root.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Component, {
+        shopifyData: props
+      }));
+      return root;
+    }
+  });
+});
+
+/***/ }),
+
+/***/ "./js/components/react/sample.js":
+/*!***************************************!*\
+  !*** ./js/components/react/sample.js ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+var sample = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Hiiii....");
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (sample);
+
+/***/ })
+
+}]);
---
+++ js/components/react/sample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const sample = () => {
+  return (
+    <div>
+      Hiiii....
+    </div>
+  )
+}
+
+export default sample
