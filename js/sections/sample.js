fa72dd63caa68a8c288b815d15bb1553b6d89ef4
Vasavi <nagavasavi@shoptrade.co>
2024-07-05 15:36:15 +0530

    Initial buid setup

---
+++ js/sections/sample.js
@@ -0,0 +1,6 @@
+import reactWrapper from "JsComponents/react-wrapper";
+import sample from "ReactComponents/sample";
+
+export default () => {
+    reactWrapper(sample, 'sample-data', '#sample-data');
+}
